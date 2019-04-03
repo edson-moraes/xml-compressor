d750917ab112781b97cd47c6de5e32ae462e86b1
Edson Moraes <edson-moraes@outlook.com>
2019-04-02 23:31:08 -0300

    Implementado um exemplo no método main

--- src/test/java/br/com/unisys/xmlexitools/XmlCompressorTest.java
+++ src/test/java/br/com/unisys/xmlexitools/XmlCompressorTest.java
@@ -5,12 +5,10 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.xml.sax.SAXException;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-import static jdk.nashorn.internal.runtime.regexp.joni.Config.log;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
@@ -19,75 +17,74 @@ import static org.junit.jupiter.api.Assertions.fail;
  */
 public class XmlCompressorTest {
 
-	private final int UNENCODED_SIZE = 1;
-	private final int MAX_COMPRESSION_ENCODED_SIZE = 1;
-	private final int MAX_COMPRESSION_DECODED_SIZE = 1;
-	private final int MAX_FIDELITY_ENCODED_SIZE = 1;
-	private final int MAX_FIDELITY_DECODED_SIZE = 1;
+    private final int UNENCODED_SIZE = 55219;
+    private final int MAX_COMPRESSION_ENCODED_SIZE = 14311;
+    private final int MAX_COMPRESSION_DECODED_SIZE = 1;
+    private final int MAX_FIDELITY_ENCODED_SIZE = 14311;
+    private final int MAX_FIDELITY_DECODED_SIZE = 1;
 
-	private File testXmlFile;
-	private InputStream testXmlInputStream;
-	private String testXml;
-	private XmlCompressor maxCompressionInstance;
-	private XmlCompressor maxFidelityInstance;
+    private static File testXmlFile;
+    private static InputStream testXmlInputStream;
+    private static String testXml;
+    private static XmlCompressor maxCompressionInstance;
+    private static XmlCompressor maxFidelityInstance;
 
 
-	@BeforeAll
-	void setUp() {
+    @BeforeAll
+    static void setUp() throws IOException {
 
-		maxCompressionInstance = XmlCompressor.getMaxCompressionInstance();
-		maxFidelityInstance = XmlCompressor.getMaxFidelityInstance();
+        String fileName = "src/main/resources/input.xml";
+        maxCompressionInstance = XmlCompressor.getMaxCompressionInstance();
+        maxFidelityInstance = XmlCompressor.getMaxFidelityInstance();
 
-		testXmlFile = new File("input.xml");
-		try (FileInputStream testInputStream = new FileInputStream(testXmlFile)) {
-			this.testXmlInputStream = testInputStream;
-			this.testXml = testXmlInputStream.toString();
+        testXmlFile = new File(fileName);
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+        byte[] fileData = Files.readAllBytes(Paths.get(fileName));
 
-	@Test
-	void encodeFromString() {
-		String maxFidelityOutput = "";
-		String maxCompressionOutput = "";
-		try {
-			maxFidelityOutput = maxFidelityInstance.encodeFromString(testXml);
-			maxCompressionOutput = maxCompressionInstance.encodeFromString(testXml);
-		} catch (EXIException | IOException | SAXException e) {
-			e.printStackTrace();
-			fail();
-		}
-		assertEquals(MAX_FIDELITY_ENCODED_SIZE, maxFidelityOutput.getBytes().length);
-		assertEquals(MAX_COMPRESSION_ENCODED_SIZE, maxCompressionOutput.getBytes().length);
-	}
+        testXml = new String(fileData);
+        testXmlInputStream = new ByteArrayInputStream(fileData);
+    }
 
-	@Test
-	public void decodeFromString() {
-	}
+    @Test
+    void encodeFromString() {
+        String maxFidelityOutput = "";
+        String maxCompressionOutput = "";
+        try {
+            maxFidelityOutput = maxFidelityInstance.encodeFromString(testXml);
+            maxCompressionOutput = maxCompressionInstance.encodeFromString(testXml);
+        } catch (EXIException | IOException | SAXException e) {
+            e.printStackTrace();
+            fail();
+        }
+        assertEquals(MAX_FIDELITY_ENCODED_SIZE, maxFidelityOutput.getBytes().length);
+        assertEquals(MAX_COMPRESSION_ENCODED_SIZE, maxCompressionOutput.getBytes().length);
+    }
 
-	@Test
-	public void encodeFromStream() {
-	}
+    @Test
+    public void decodeFromString() {
+    }
 
-	@Test
-	public void decodeFromStream() {
-	}
+    @Test
+    public void encodeFromStream() {
+    }
 
-	@Test
-	public void encodeFromFile() {
-	}
+    @Test
+    public void decodeFromStream() {
+    }
 
-	@Test
-	public void encodeFromFile1() {
-	}
+    @Test
+    public void encodeFromFile() {
+    }
 
-	@Test
-	public void decodeFromFile() {
-	}
+    @Test
+    public void encodeFromFile1() {
+    }
 
-	@Test
-	public void decodeFromFile1() {
-	}
+    @Test
+    public void decodeFromFile() {
+    }
+
+    @Test
+    public void decodeFromFile1() {
+    }
 }
